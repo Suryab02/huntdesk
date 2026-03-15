1a314d4e795dc8ad1cdc95a5ab9db88654b70357
Suryab02 <bsprabhas2002@gmail.com>
2026-03-15 18:53:18 +0530

    Fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,14 +14,5 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          charts: ['recharts'],
-          dnd: ['@hello-pangea/dnd'],
-        }
-      }
-    }
   }
 })
