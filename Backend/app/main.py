27108e05474528a462c5c8b6e8ab1d3d09bc24e4
Suryab02 <bsprabhas2002@gmail.com>
2026-03-15 18:47:12 +0530

    Update API baseURL to use env variable

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -18,12 +18,8 @@ app = FastAPI(
 # Allow React frontend to talk to this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
